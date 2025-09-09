481423142e8c4dd8bc1f46c740472273c681515c
didasmba <daniel.mbarushi@gmail.com>
2025-09-09 11:59:25 +0300

    App and Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,27 @@
 //   return <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
 // }
 
-function Profile() {
-  return <img src="https://i.imgur.com/QIrZWGIs.jpg" alt="Alan L. Hart" />;
-}
+// function Profile() {
+//   return <img src="https://i.imgur.com/QIrZWGIs.jpg" alt="Alan L. Hart" />;
+// }
 
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  );
+// export default function Gallery() {
+//   return (
+//     <section>
+//       <h1>Amazing scientists</h1>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//     </section>
+//   );
+// }
+
+// export default function Congratulations() {
+//   return <h1>salle sonorite euphorie la sacem de laurant pagny</h1>;
+// }
+
+import Gallery from "./Gallery";
+
+export default function App() {
+  return <Gallery />;
 }
